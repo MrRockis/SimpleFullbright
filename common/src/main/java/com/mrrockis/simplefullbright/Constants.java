0c8c6392faa6a84ff0d7968fdacfe774dbb6ce7f
MrRockis <rockis167@gmail.com>
2025-12-13 02:00:58 +0200

    Updated to Minecraft 1.21.11

--- common/src/main/java/com/mrrockis/simplefullbright/Constants.java
+++ common/src/main/java/com/mrrockis/simplefullbright/Constants.java
@@ -2,7 +2,7 @@ package com.mrrockis.simplefullbright;
 
 import com.mojang.blaze3d.platform.InputConstants;
 import net.minecraft.client.KeyMapping;
-import net.minecraft.resources.ResourceLocation;
+import net.minecraft.resources.Identifier;
 import org.lwjgl.glfw.GLFW;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -14,7 +14,7 @@ public class Constants {
     public static final Logger LOG = LoggerFactory.getLogger(MOD_NAME);
 
     public static final KeyMapping.Category KEY_CATEGORY =
-            new KeyMapping.Category(ResourceLocation.fromNamespaceAndPath("simplefullbright", "main"));
+            new KeyMapping.Category(Identifier.fromNamespaceAndPath("simplefullbright", "main"));
 
     public static final KeyMapping TOGGLE_KEY = new KeyMapping(
             "key.simplefullbright.toggle",
